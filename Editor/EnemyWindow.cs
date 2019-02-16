ff447cfe5260acb5588b5b9e5af79f352f916d11
Dmitriy <Onecard1c@gmail.com>
2019-02-16 13:08:51 +0700

    Создал окно, для настройки врагов

---
+++ Editor/EnemyWindow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace DM.Editor
+{
+    public class EnemyWindow : EditorWindow
+    {
+        Color enemyColor;
+
+        [MenuItem("GameObject/Enemy Attributes")]
+        public static void ShowWindow()
+        {
+            GetWindow<EnemyWindow>("Enemy Window");
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Attributes", EditorStyles.boldLabel);
+
+            enemyColor = EditorGUILayout.ColorField("Color", enemyColor);
+
+            if (GUILayout.Button("Change color"))
+            {
+                ChangeColor();
+            }            
+        }
+
+        private void ChangeColor()
+        {
+            foreach (GameObject enemy in Selection.gameObjects)
+            {
+                Renderer rend = enemy.GetComponent<Renderer>();
+                if (rend != null)
+                {
+                    rend.material.color = enemyColor;
+                }
+            }
+        }
+    }
+}
+
