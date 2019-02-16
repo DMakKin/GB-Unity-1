8f72bf4b8c9d498f12e07d8b6514efcfd20b402e
Dmitriy <Onecard1c@gmail.com>
2019-02-16 13:05:38 +0700

    Создал класс Enemy

---
+++ Model/AI/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+
+namespace DM
+{
+    public class Enemy : BaseObject
+    {
+        private float _lookRadius = 10f;
+        [HideInInspector] public NavMeshAgent _enemyAgent;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _enemyAgent = GetComponent<NavMeshAgent>();
+        }
+        
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _lookRadius);
+        }
+
+    }
+}
