fb8f3ca707848cbf4e480c3a77a52c25b58983f1
Dmitriy <Onecard1c@gmail.com>
2019-02-16 13:08:22 +0700

    Класс для зрения врага, в работе

---
+++ Model/AI/FieldOfView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+
+namespace DM
+{
+    public class FieldOfView
+    {
+        private float _lookDistance = 10f;
+        private float _lookAngle = 70f;               
+
+    }
+}
