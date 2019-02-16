8e1e52541ac489c32fe2f4fa251ad375850e5abc
Dmitriy <Onecard1c@gmail.com>
2019-02-16 13:07:33 +0700

    Переместил скрипт

---
+++ Model/AI/SpotOfSummon.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace DM
+{
+    public class SpotOfSummon : BaseObject
+    {
+
+        public ParticleSystem _spot;
+        public Creature _summonPrefab;
+        [HideInInspector] public SpotOfSummon _spotOfSummon;
+        public LayerMask _maskForSummon;
+        [HideInInspector] public bool IsActive = false;
+        [HideInInspector] public Creature _chosenCreature;        
+
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            _spotOfSummon = FindObjectOfType<SpotOfSummon>();
+        }
+
+        public void EnableEffect()
+        {
+            if (IsActive)
+            {
+                _spotOfSummon.gameObject.SetActive(false);
+                IsActive = false;
+            }
+            else
+            {
+                _spotOfSummon.gameObject.SetActive(true);
+                IsActive = true;
+            }
+        }
+
+        public void SummonCreature()
+        {
+            _chosenCreature = Instantiate(_summonPrefab, _spot.transform.position, _spot.transform.rotation);
+        }
+    }
+}
