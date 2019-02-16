28edb5f90d144b81ba709a0c88e4fb4b1600a565
Dmitriy <Onecard1c@gmail.com>
2019-02-16 13:09:46 +0700

    Закомментировал лишнее

---
+++ Model/AI/Creature.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace DM
+{
+    public class Creature : BaseObject
+    {
+        [HideInInspector] public NavMeshAgent _creatureAgent;
+        [HideInInspector] public bool _goToPlayer = true;
+        [HideInInspector] public PlayerStamina _player;
+        private bool _alreadyChecking = false;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _creatureAgent = GetComponent<NavMeshAgent>();
+            _player = FindObjectOfType<PlayerStamina>();
+        }
+
+        public void CheckDestination()
+        {
+            if (!_creatureAgent.hasPath && !_alreadyChecking)
+            {
+                _alreadyChecking = true;
+                //Debug.Log("Отcчет пошел!");
+                Invoke(nameof(ReadyToGoBack), 5f);
+            }
+        }
+
+        private void ReadyToGoBack()
+        {
+            _alreadyChecking = false;
+            _goToPlayer = true;
+        }
+    }
+}
