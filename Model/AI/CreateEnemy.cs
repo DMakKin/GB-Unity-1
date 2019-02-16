13a79cf2555956afe78366dc53b825bf4a2f1dbf
Dmitriy <Onecard1c@gmail.com>
2019-02-16 13:10:09 +0700

    Класс создания Enemy из инспектора

---
+++ Model/AI/CreateEnemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DM
+{
+    public class CreateEnemy : MonoBehaviour
+    {
+        [SerializeField] private Enemy _enemyPrefab;
+        public void InstantiateEnemy(Vector3 position)
+        {
+            Instantiate(_enemyPrefab, position, Quaternion.identity);
+        }
+    }
+}
