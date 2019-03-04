0b87b02969f980a21ab5c0a6891796dd6b0c6ed4
Dmitriy <Onecard1c@gmail.com>
2019-03-05 04:58:43 +0700

    Добавил взрыв от снаряда

--- Model/FireBall.cs
+++ Model/FireBall.cs
@@ -4,10 +4,29 @@ namespace DM
 {
     public class FireBall : Magic
     {
+        [SerializeField] protected ParticleSystem explEffect;
+        private float _radius = 6;
+        private float _force = 600;
+
         private void OnCollisionEnter(Collision collision)
         {
-            //Debug.Log("Boom!");
+            Explosion();
             Destroy(gameObject);
+        }
+
+        private void Explosion()
+        {
+            Instantiate(explEffect, transform.position, transform.rotation);
+            Collider[] colladers = Physics.OverlapSphere(transform.position, _radius);
+
+            foreach (Collider nearObj in colladers)
+            {
+                Rigidbody rb = nearObj.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(_force, transform.position, _radius);
+                }
+            }
         }
     }
 }
