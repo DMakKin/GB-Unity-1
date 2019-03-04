5b3128bc6226dc0e4347d398951d2401e3b5f066
Dmitriy <Onecard1c@gmail.com>
2019-03-05 04:55:44 +0700

    Удаление эффекта

---
+++ Model/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DM
+{   
+    public class Explosion : MonoBehaviour
+    {
+        [SerializeField] protected float _timeToDestruct = 1;
+
+        void Start()
+        {
+            Destroy(gameObject, _timeToDestruct);
+        }
+    }
+}
